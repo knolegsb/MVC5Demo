0823530aa128cdad5b666938518dca2608c496f0
knolegsb <seon2you@hotmail.com>
2016-03-09 00:12:05 -0800

    done

---
+++ MVC5Demo/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using MVC5Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Demo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Create(int departmentId)
+        {
+            var employee = new Employee();
+            employee.Department_Id = departmentId;
+            return View(employee);
+        }
+
+        MVC5DemoDatabaseEntities _db = new MVC5DemoDatabaseEntities();
+
+        [HttpPost]
+        public ActionResult Create(Employee employee1)
+        {
+            if (ModelState.IsValid)
+            {
+                var department = _db.Departments.Single(d => d.Id == employee1.Department_Id);
+                department.Employees.Add(employee1);
+                _db.SaveChanges();
+                return RedirectToAction("Details", "Home", new { id = employee1.Department_Id });
+            }
+
+            return View(employee1);
+        }
+    }
+}
---
+++ MVC5Demo/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Demo.Models;
+
+namespace MVC5Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        MVC5DemoDatabaseEntities db = new MVC5DemoDatabaseEntities();
+
+        public ActionResult Index()
+        {
+            var alldepartment = db.Departments;
+            return View(alldepartment);
+        }
+
+        public ActionResult About()
+        {
+            var model = new Person() { Name = "Sean" };
+
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Location = "Seoul, Korea";
+            ViewBag.Name = "Sean You";
+            ViewBag.Age = 39;
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            var selectedDepartment = db.Departments.Single(d => d.Id == id);
+
+            return View(selectedDepartment);
+        }
+
+        public ActionResult Create()
+        {
+            return View(new Department());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Department department)
+        {
+            db.Departments.Add(department);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
